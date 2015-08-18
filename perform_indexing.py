86868cb01cf809590f804897fb304ee5f3c4f3e0
Sergey Venev <sergpolly@gmail.com>
2015-08-17 23:48:08 -0400

    perform_indexing made universal for genbank fasta

--- perform_indexing.py
+++ perform_indexing.py
@@ -1,15 +1,27 @@
+import sys
 from Bio import SeqIO
 
+# see corresponding description in the project's wiki
 
-inventory = 'genbank.inventory'
-db_file = 'genbank.idx'
+inventory = sys.argv[1]
+db_file = sys.argv[2]
+seq_format = sys.argv[3]
+
+# sanity check is ommited for such a short and simple script
 
 
-# get the files names to be indexed ...
+# get the files names to be indexed
 with open(inventory,'r') as fp:
     fnames = [line.strip() for line in fp.readlines()]
 
-# index em!
-res = SeqIO.index_db(db_file,filenames=fnames,format='genbank')
+# index them depending on the format
+if format == "genabnk":
+    res = SeqIO.index_db(db_file,filenames=fnames,format=seq_format)
+elif format == "fasta":
+    get_index = lambda name: name.split('|')[3]
+    res = SeqIO.index_db(db_file,filenames=fnames,format=seq_format,key_function=get_index)
+else:
+    print "Only genbank and fasta formats are accepted!"
+    sys.exit(1)
 
-#quit ...
+
